dada7541f42512de1aacf950265e1697f4a1374e
Sumit Bansal <sbansal.mait@gmail.com>
2022-01-15 00:05:46 +0530

    Naming convention changed for some methods.

--- app/helpers/format_helper.rb
+++ app/helpers/format_helper.rb
@@ -4,15 +4,15 @@ module FormatHelper
   AVAILABLE_FORMAT = { dollar_format: '$', percent_format: '%' }
 
   def get_users_data(params)
-    params = convert_all_format_to_coma(params)
-    csv_data = generate_csv_file_with_combine_data(params)
+    params = convert_all_format_to_coma_separated(params)
+    generate_user_data(params)
   end
 
   def convert_to_comma_separated_data(format_type, str)
     str.gsub(format_type, ',')
   end
 
-  def convert_all_format_to_coma(params)
+  def convert_all_format_to_coma_separated(params)
     params.keys.reduce({}) do |hash, key|
       hash[key] = if check_format_available?(key)
         convert_to_comma_separated_data(AVAILABLE_FORMAT[key], params[key])
@@ -27,7 +27,7 @@ module FormatHelper
     AVAILABLE_FORMAT.keys.include?(sym)
   end
 
-  def generate_csv_file_with_combine_data(params)
+  def generate_user_data(params)
     params.keys.reduce([]) do |arr, key|
       if check_format_available?(key)
         csv = CsvHandler.new
